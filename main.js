e6590e6d4d01ae624751a529d1d9d88dd0d867dd
Lauren Brizel <lauren.brizel@ufl.edu>
2021-10-31 14:29:48 -0700

    Add weather description js

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-var formE1 = docment.querySelector('form')
+var formE1 = document.querySelector('form')
 var inputE1 = document.querySelector('input')
 var weatherE1 = document.getElementById('weather')
 console.log (formE1, inputE1, weatherE1)
@@ -10,42 +10,26 @@ formE1.onsubmit = function(e) {
     e.preventDefault()
     var weatherQuery = inputE1.value.trim()
     if (!weatherQuery) return
-    fetch(api.openweathermap.org/data/2.5/weather?q=YOURQUERY&units=imperial&appid=1dcd4bcfc67e05de26c5c34496a4fe7e)
-.then(function(res){
-    return res.json()
-})
-.then(function(res){
-    console.log(res) {
-        renderWeather(res)
-        inputE1.value= ""
-})
-.catch (fucntion(err){
+    fetch("https://api.openweathermap.org/data/2.5/weather?appid=b3989a6cae8c8d75a992609a4373766d&units=imperial&q=" + weatherQuery)
+    .then(function(res) {
+     return res.json()
+    })
+    .then(function(res) {
+     renderWeather(res)
+     inputE1.value = ""
+    })
+    .catch (function(err){
     console.log(err)
-})
+    })
 }
-
+//
 function renderWeather(weatherObj) {
-    //clear previous weather
+    //clear previous 
     weatherE1.innerHTML = ""
-    //handle weather not found
-    if (weatherObj.Response ===='False'){
-        weatherE1.textContent = 'Weather not found'
-        return
+    //weather description
+    weatherObj.weather.forEach(function(weather) {
+        var weather_descrip = document.createElement('h2')
+        weather_descrip.textContent = weather.description
+        weatherE1.appendChild(weather_descrip)
     }
-
-//render current weather 
-var currentWeatherE1 = document.createElement('h2')
-currentWeatherE1.textContent = weatherObj.weather
-weatherE1.appendChild(currentWeatherE1)
-
-//render weather icon
-var weathericon = document.createElement('img')
-temperature.textContent = 'Current' + weatherObj.main.temp
-weatherE1.appendChild(temperature)
-
-//render feels like temp
-var feelsLike == document.createElement('p')
-feelsLike.style.fontStyle = 'italic'
-feelsLike.textContent = 'Feels like' + weatherObj.main.feels_like
-weatherE1.appendChild(feels)
-}
+    )}
