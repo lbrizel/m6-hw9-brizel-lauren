3ba135b875ead9b636b53564f6e0ec4d9869be23
Lauren Brizel <lauren.brizel@ufl.edu>
2021-10-31 15:27:28 -0700

    Add additional js

--- main.js
+++ main.js
@@ -12,11 +12,11 @@ formE1.onsubmit = function(e) {
     if (!weatherQuery) return
     fetch("https://api.openweathermap.org/data/2.5/weather?appid=b3989a6cae8c8d75a992609a4373766d&units=imperial&q=" + weatherQuery)
     .then(function(res) {
-     return res.json()
+    return res.json()
     })
     .then(function(res) {
-     renderWeather(res)
-     inputE1.value = ""
+    renderWeather(res)
+    inputE1.value = ""
     })
     .catch (function(err){
     console.log(err)
@@ -31,5 +31,28 @@ function renderWeather(weatherObj) {
         var weather_descrip = document.createElement('h2')
         weather_descrip.textContent = weather.description
         weatherE1.appendChild(weather_descrip)
-    }
-    )}
+    })
+    //icon
+    weatherObj.weather.forEach(function(weatherIcon) {
+        var icon = document.createElement("img")
+        icon.src = weather.icon
+        weatherE1.appendChild(icon)
+    })
+
+    //current temp
+    var currentTemp = document.createElement('p')
+    currentTemp.textContent = "Current Temperature: " + weatherObj.main.temp + "°F"
+    weatherE1.appendChild(currentTemp)
+
+    //feels like
+    var feelsLike = document.createElement('p')
+    feelsLike.textContent = "Feels Like: " + weatherObj.main.feels_like + "°F"
+    feelsLike.style.fontStyle = 'italic'
+    weatherE1.appendChild(feelsLike)
+
+    //last update
+    var timestamp = document.createElement('p')
+    timestamp.textContent = "Last updated: " + Date().toLocaleTimeString()
+    weatherE1.appendchild(timestamp)
+
+}
